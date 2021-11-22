349bf009715a526656b049b30ace8e7b68b7033f
AMuzykus <andrew.muzikus@gmail.com>
2021-11-22 21:49:18 +0300

    Add 2 buttons(random number and dialogue) and their functions.

--- main.py
+++ main.py
@@ -8,16 +8,50 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("🎲 Рандомное число")
+    item2 = types.KeyboardButton("😊 Как дела?")
+
+    markup.add(item1, item2)
     bot.send_message(message.chat.id, "Добро пожаловать,"
                                       " {0.first_name}!\nЯ - <b>{1.first_name}</b>,"
                                       " бот созданный чтобы быть подопытным кроликом."
                      .format(message.from_user, bot.get_me()),
-                     parse_mode='html')
+                     parse_mode='html', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def echo_message(message):
-    welcome(message)
+    if message.chat.type == 'private':
+        if message.text == '🎲 Рандомное число':
+            bot.send_message(message.chat.id, str(random.randint(0, 100)))
+        elif message.text == '😊 Как дела?':
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+            markup.add(item1, item2)
+            bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
+        else:
+            welcome(message)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'good':
+                bot.send_message(call.message.chat.id, 'Вот и отличненько 😊')
+            elif call.data == 'bad':
+                bot.send_message(call.message.chat.id, 'Бывает 😢')
+
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="😊 Как дела?",
+                                  reply_markup=None)
+
+            bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
+                                      text="Уведомление от бота^^")
+
+    except Exception as e:
+        print(repr(e))
 
 
 bot.polling(none_stop=True)
