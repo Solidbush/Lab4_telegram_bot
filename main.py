e0b4ac7eea49e90dda091c8d85d0648c19cd0a06
AMuzykus <andrew.muzikus@gmail.com>
2021-11-22 21:21:43 +0300

    add files

---
+++ main.py
@@ -0,0 +1,23 @@
+import telebot
+import config
+import random
+from telebot import types
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, "Добро пожаловать,"
+                                      " {0.first_name}!\nЯ - <b>{1.first_name}</b>,"
+                                      " бот созданный чтобы быть подопытным кроликом."
+                     .format(message.from_user, bot.get_me()),
+                     parse_mode='html')
+
+
+@bot.message_handler(content_types=['text'])
+def echo_message(message):
+    welcome(message)
+
+
+bot.polling(none_stop=True)
